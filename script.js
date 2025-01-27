5827414c248c80e8c9111b53a25536b48cbb6fac
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2025-01-27 14:24:13 -0500

    se agrega el cuarto ejercicio de logica de programacion llamado do-while

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+const scrollNav = document.querySelector('.nav__links')
+
+//Tabbed component 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//nav y nav
+const nav = document.querySelector('.nav')
+const header = document.querySelector('.header');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -29,3 +41,85 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Desplazarnos la seccion   al hacer click de la pagina
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+
+// Navegacion por la pagina usando delegacion de eventos 
+
+scrollNav.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth', block: 'center' })
+  };
+
+});
+
+// Tabbed component usando delegacion de eventos y DOM Traversing
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+});
+
+// Menu animation hover 
+
+const handleHover = function (e) {
+
+  if (e.target.classList.contains('nav__link')) {
+
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+
+  };
+
+}
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+//Barra de navegacion fija al navegar en la pagina usando la API de IntersectionObserver
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+
+}
+
+const headerObserver = new IntersectionObserver(stickyNav,
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+  });
+
+headerObserver.observe(header);
+
+
+
+
+
